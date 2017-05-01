ca66b0ebfde05ba3c4a81c7fd8083217eb8314eb
GracieChristine <graciechristineli@gmail.com>
2017-05-01 10:54:18 -0600

    Update some styling.

--- app/jobs/instadash.rb
+++ app/jobs/instadash.rb
@@ -2,12 +2,12 @@ require 'instagram'
 
 # Instagram Client ID from http://instagram.com/developer
 Instagram.configure do |config|
-  config.client_id = ''
+  config.client_id = '61c474f3c51d40e48be9be0c5e104809'
 end
 
 # Latitude, Longitude for location
-instadash_location_lat = '45.429522'
-instadash_location_long = '-75.689613'
+instadash_location_lat = '40.01499'
+instadash_location_long = '-105.27055'
 
 Dashing.scheduler.every '10m', :first_in => 0 do |job|
   photos = Instagram.media_search(instadash_location_lat,instadash_location_long)
